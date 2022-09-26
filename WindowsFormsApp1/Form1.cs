4503476b3f1b3bf9fb3590f01e7befb4a1ad5c43
konst <qwertyuiop12345jomh@gmail.com>
2022-09-26 18:30:45 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            List<string> TestList = new List<string> { "12", "15", "18", "21", "24" };
+            listBox1.DataSource = TestList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Index = listBox1.SelectedIndex;
+            if (Index.Equals(0))
+            {
+                var nums = Program.F12(Convert.ToInt16(textBox1.Text), Convert.ToInt16(textBox2.Text));
+                label3.Text = "X= " + nums[0] + " Y= " + nums[1];
+            } 
+            else if (Index.Equals(1))
+            {
+                var nums = Program.F15(Convert.ToInt16(textBox1.Text), Convert.ToInt16(textBox2.Text));
+                label3.Text = "X= " + nums[0] + " Y= " + nums[1];
+            } 
+            else if (Index.Equals(2))
+            {
+                var nums = Program.F18(Convert.ToInt16(textBox1.Text), Convert.ToInt16(textBox2.Text), Convert.ToInt16(textBox3.Text),
+                    Convert.ToInt16(textBox4.Text), Convert.ToInt16(textBox5.Text), Convert.ToInt16(textBox6.Text));
+                label3.Text = "X=" + nums[0] + " Y= " + nums[1];
+            } 
+            else if (Index.Equals(3))
+            {
+                var num = Program.F21(Convert.ToInt16(textBox1.Text), Convert.ToInt16(textBox2.Text), Convert.ToInt16(textBox3.Text));
+                label3.Text = "MAX= " + num;
+            }
+            else if (Index.Equals(4))
+            {
+                label3.Text = Program.f24(Convert.ToInt16(textBox1.Text), Convert.ToInt16(textBox2.Text));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Index = listBox1.SelectedIndex;
+            if (Index.Equals(2))
+            {
+                label4.Text = "X*";
+                label5.Text = "Y*";
+                label6.Text = "=";
+                label6.Show();
+                label7.Show();
+                label8.Show();
+                label9.Show();
+                textBox3.Show();
+                textBox4.Show();
+                textBox5.Show();
+                textBox6.Show();
+            }
+            else if (Index.Equals(3))
+            {
+                label6.Show();
+                textBox3.Show();
+                label4.Text = "X=";
+                label5.Text = "Y=";
+                label6.Text = "Z=";
+                label7.Hide();
+                label8.Hide();
+                label9.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+                textBox6.Hide();
+            }
+            else
+            {
+                label4.Text = "X=";
+                label5.Text = "Y=";
+                label6.Text = "Z=";
+                label6.Hide();
+                label7.Hide();
+                label8.Hide();
+                label9.Hide();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+                textBox6.Hide();
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static List<double> F12(int x, int y)
+        {
+            var nums = new List<double>();
+            if (x > 0){
+                nums.Add(x * 2);
+                if (y > 0)
+                {
+                    nums.Add(y * 2);
+                    return nums;
+                }
+                else
+                {
+                    nums.Add(Math.Pow(y, 2));
+                    return nums;
+                }
+            }
+            else
+            {
+                nums.Add(Math.Pow(x, 2));
+                if (y > 0)
+                {
+                    nums.Add(y * 2);
+                    return nums;
+                }
+                else
+                {
+                    nums.Add(Math.Pow(y, 2));
+                    return nums;
+                }
+            }
+        }
+
+        public static List<double> F15(int x, int y)
+        {
+            var nums = new List<double>();
+            if (x > 0)
+            {
+                nums.Add(Math.Pow(x, 3));
+                if (y > 0)
+                {
+                    nums.Add(Math.Pow(y, 3));
+                    return nums;
+                }
+                else
+                {
+                    nums.Add(Math.Abs(y));
+                    return nums;
+                }
+            }
+            else
+            {
+                nums.Add(Math.Abs(x));
+                if (y > 0)
+                {
+                    nums.Add(Math.Pow(y, 3));
+                    return nums;
+                }
+                else
+                {
+                    nums.Add(Math.Abs(y));
+                    return nums;
+                }
+            }
+        }
+
+        public static List<double> F18(double x1, double y1, double c1, double x2, double y2, double c2)
+        {
+            double D = (x1 * y2) - (x2 * y1);
+            var nums = new List<double>();
+            if (D != 0)
+            {
+                double x = ((c1 * y2) - (c2 * y1)) / D;
+                double y = ((x1 * c2) - (x2 * c1)) / D;
+                nums.Add(x);
+                nums.Add(y);
+                return nums;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static int F21(int x, int y, int z)
+        {
+            if (x > y && x > z)
+            {
+                return x;
+            } 
+            else if (y > z && y > x)
+            {
+                return y;
+            } 
+            else
+            {
+                return z;
+            }
+        }
+
+        public static string f24(int x, int y)
+        {
+            if ((x > 0 && y < 0) || (x < 0 && y > 0))
+            {
+                return "" + x * y;
+            }
+            else
+            {
+                return "X= " + Math.Pow(x, 2) + " Y=" + Math.Pow(y, 2);
+            }
+        }
+    }
+}
